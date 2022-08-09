7ca11ef1c2888dea5e8cf8c03d750ca96202371d
SoftwareTestingo <admin@softwaretestingo.com>
2022-08-09 23:51:27 +0530

    Hashcode Program Updated

--- Java_Programs/src/com/softwaretestingo/basic/HashcodeExample.java
+++ Java_Programs/src/com/softwaretestingo/basic/HashcodeExample.java
@@ -3,16 +3,11 @@ public class HashcodeExample
 {
 	public static void main(String[] args) 
 	{
-		//Create integer object  
-        Integer i = new Integer("789456");  
-        //Returned hash code value for this object   
-        int hashValue = i.hashCode();  
-        System.out.println("Hash code Value for object is: " + hashValue); 
-        
-        HashcodeExample obj=new HashcodeExample();
-        HashcodeExample obj1=new HashcodeExample();
-        
+		int hashValue = Integer.hashCode(185);  
+		System.out.println("Hash code Value for object is: " +hashValue); 
+
+		HashcodeExample obj=new HashcodeExample();        
 		System.out.println("Hashcode Value Of Obj: "+obj.hashCode());
-		System.out.println("Hashcode Value Of Obj1: "+obj1.hashCode());
+
 	}
 }
