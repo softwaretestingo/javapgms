727015da1503c2c5a3880d6362d083ae663d06b2
SoftwareTestingo <admin@softwaretestingo.com>
2022-08-13 08:43:03 +0530

    Swap Numbers Programs Added

---
+++ Java_Programs/src/com/softwaretestingo/basic/SwapNumbersEx5.java
@@ -0,0 +1,15 @@
+package com.softwaretestingo.basic;
+public class SwapNumbersEx5 
+{
+	public static void main(String[] args) 
+	{
+	    int a=5;
+	    int b=4;
+	    
+	    System.out.println("Before Swap the Value Of A & B: "+a+" "+b);
+	   
+	    b=a+b -(a=b);
+	    
+	    System.out.println("After Swap the Value Of A & B: "+a+" "+b);
+	}
+}
