46e1c67a4df240f4bc1cd5a3250d0dedebf07aaa
SoftwareTestingo <admin@softwaretestingo.com>
2022-08-13 20:08:16 +0530

    Decimal To Binary Programs

---
+++ Java_Programs/src/com/softwaretestingo/basic/DecimaltoBinaryEx3.java
@@ -0,0 +1,29 @@
+package com.softwaretestingo.basic;
+import java.util.Scanner;
+import java.util.Stack;
+public class DecimaltoBinaryEx3 
+{
+	public static void main(String[] args) 
+	{
+		// Create Stack object
+		Stack<Integer> stack = new Stack<Integer>();
+
+		Scanner sc = new Scanner(System.in);
+		// User input 
+		System.out.println("Enter decimal number: ");
+		int num = sc.nextInt();
+
+		while (num != 0)
+		{
+			int d = num % 2;
+			stack.push(d);
+			num /= 2;
+		} 
+		System.out.print("\nBinary representation is:");
+		while (!(stack.isEmpty() ))
+		{
+			System.out.print(stack.pop());
+		}
+		System.out.println();
+	}
+}
