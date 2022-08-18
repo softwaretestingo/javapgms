35d1dd7975134af48bd053eee7a7c8c3ba3f2f38
SoftwareTestingo Admin <softwaretestingo@email.com>
2022-08-18 22:14:30 +0530

    This Keyword Programs Added

---
+++ Java_Programs/src/com/softwaretestingo/basic/ThisKeywordEx4.java
@@ -0,0 +1,23 @@
+package com.softwaretestingo.basic;
+class ex5
+{  
+	void ab(ex5 t) 
+	{ 
+		System.out.println("Ab Method called"); 
+	} 
+	void ac()  
+	{ 
+		/* Passing this keyword as an argument in the ab method and it will 
+		 * pass the reference of current class object to the ab method.
+		 */
+		ab(this);  
+	} 
+}  
+public class ThisKeywordEx4 
+{
+	public static void main(String[] args) 
+	{
+		ex5 obj=new ex5();
+		obj.ac();
+	}
+}
