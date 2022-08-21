3913660f6a7b8c9f1452b1f7ac0ddd9aa3bd6164
SoftwareTestingo Admin <softwaretestingo@email.com>
2022-08-21 20:27:01 +0530

    Palindrome Number Programs Added

---
+++ Java_Programs/src/com/softwaretestingo/interviewprograms/PalindromeNumberEx4.java
@@ -0,0 +1,29 @@
+package com.softwaretestingo.interviewprograms;
+import java.util.Scanner;
+public class PalindromeNumberEx4 
+{
+	public static void main(String[] args) 
+	{
+		String originalString, finalString="";
+		int length;
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a String: ");
+		originalString=sc.nextLine();
+		
+		length=originalString.length();
+		
+		for(int i=length-1; i>=0; i--)
+		{
+			finalString=finalString+originalString.charAt(i);
+		}
+		
+		// Two Value Comparison
+		if (originalString.equalsIgnoreCase(finalString))
+		{
+			System.out.println(originalString+ " is a Palindrome");
+		}
+		else
+			System.out.println(originalString+ " is Not a Palindrome");
+	}
+}
