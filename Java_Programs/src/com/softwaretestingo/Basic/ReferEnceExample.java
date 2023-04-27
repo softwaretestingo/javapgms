1a3913bc577030e07e074da12241a61a7836d41b
SoftwareTestingo Admin <softwaretestingo@email.com>
2023-04-27 08:40:12 +0530

    Variable Examples Updated

---
+++ Java_Programs/src/com/softwaretestingo/Basic/ReferEnceExample.java
@@ -0,0 +1,19 @@
+package com.softwaretestingo.Basic;
+class a112
+{
+	int i=10;
+}
+class b112 extends a112
+{
+	int i=20;
+	///int j=40;
+}
+public class ReferEnceExample 
+{
+	public static void main(String[] args) 
+	{
+		a112 obj=new b112();
+		System.out.println(obj.i);
+		//System.out.println(obj.j); //Newly added variables in child class are not accessable
+	}
+}
