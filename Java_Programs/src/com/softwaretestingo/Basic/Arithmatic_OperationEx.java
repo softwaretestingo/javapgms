9db23ce6e11359edadc7a05ccf24407c5ec9d389
SoftwareTestingo Admin <softwaretestingo@email.com>
2023-04-29 12:50:43 +0530

    Method Programs Updated

---
+++ Java_Programs/src/com/softwaretestingo/Basic/Arithmatic_OperationEx.java
@@ -0,0 +1,34 @@
+package com.softwaretestingo.Basic;
+abstract class operations
+{
+	abstract public void printinfo();
+}
+class addition extends operations
+{
+	public void printinfo()
+	{
+		int a=110;
+		int b=20;
+		System.out.println("The Sum Of Values : "+(a+b));
+	}
+}
+class subsctraction extends operations
+{
+	public void printinfo()
+	{
+		int a=110;
+		int b=20;
+		System.out.println("The Substraction Of Values : "+(a-b));
+	}
+}
+public class Arithmatic_OperationEx 
+{
+	public static void main(String[] args) 
+	{
+		operations obj=new addition();
+		obj.printinfo();
+		
+		operations  obj1=new subsctraction();
+		obj1.printinfo();
+	}
+}
