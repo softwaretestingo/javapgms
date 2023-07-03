5d388440f48ab93f6ec81358030bf2e395e5687e
SoftwareTestingo Team <ZeroCool@DESKTOP-EN1C21U>
2023-07-03 18:24:54 +0530

    Few Modification

--- Java_Programs/src/com/softwaretestingo/interviewprograms/FindMissingDuplicateNumberEX1.java
+++ Java_Programs/src/com/softwaretestingo/interviewprograms/FindMissingDuplicateNumberEX1.java
@@ -1,32 +1,33 @@
 package com.softwaretestingo.interviewprograms;
-
 public class FindMissingDuplicateNumberEX1 
 {
 	static void printTwoElements(int[] arr, int n)
     {
-        int[] temp
-            = new int[n]; // Creating temp array of size n
-                          // with initial values as 0.
+        int[] temp = new int[n]; // Creating temp array of size n
+        
+        // with initial values as 0.
         int repeatingNumber = -1;
         int missingNumber = -1;
  
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             temp[arr[i] - 1]++;
-            if (temp[arr[i] - 1] > 1) {
+            if (temp[arr[i] - 1] > 1) 
+            {
                 repeatingNumber = arr[i];
             }
         }
-        for (int i = 0; i < n; i++) {
-            if (temp[i] == 0) {
+        for (int i = 0; i < n; i++) 
+        {
+            if (temp[i] == 0) 
+            {
                 missingNumber = i + 1;
                 break;
             }
         }
  
-        System.out.println("The repeating number is "
-                           + repeatingNumber + ".");
-        System.out.println("The missing number is "
-                           + missingNumber + ".");
+        System.out.println("The repeating number is " + repeatingNumber + ".");
+        System.out.println("The missing number is " + missingNumber + ".");
     }
 
 	public static void main(String[] args) 
--- Java_Programs/src/com/softwaretestingo/interviewprograms/FindMissingDuplicateNumberEX2.java
+++ Java_Programs/src/com/softwaretestingo/interviewprograms/FindMissingDuplicateNumberEX2.java
@@ -7,7 +7,8 @@ public class FindMissingDuplicateNumberEX2
         int i;
         System.out.print("The repeating element is ");
  
-        for (i = 0; i < size; i++) {
+        for (i = 0; i < size; i++) 
+        {
             int abs_val = Math.abs(arr[i]);
             if (arr[abs_val - 1] > 0)
                 arr[abs_val - 1] = -arr[abs_val - 1];
@@ -16,7 +17,8 @@ public class FindMissingDuplicateNumberEX2
         }
  
         System.out.print("and the missing element is ");
-        for (i = 0; i < size; i++) {
+        for (i = 0; i < size; i++) 
+        {
             if (arr[i] > 0)
                 System.out.println(i + 1);
         }
