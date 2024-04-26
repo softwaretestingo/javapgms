d53f035deb4021010382f2527aadf29db2018a05
SoftwareTestingO <softwaretestingblog.in@gmail.com>
2024-04-26 09:23:41 +0530

    Encore Java Program Added

---
+++ Java_Programs/src/com/softwaretestingo/interviewprograms/InterviewPrograms_111_PrintSecondLastCharacterOfEachWord.java
@@ -0,0 +1,37 @@
+package com.softwaretestingo.interviewprograms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InterviewPrograms_111_PrintSecondLastCharacterOfEachWord 
+{
+	/**
+	 * Author: SoftwareTestingo Admin
+	 * Blog: www.softwaretestingo.com
+	 * Problem Statement:
+	 * How you Find the Second-to-last Character of Each word in a given String?
+	 * Company - Encora
+	 * 
+	 * Input: This ia a Sample String
+	 * Output: iiln
+	 * 
+	 * URL: https://www.softwaretestingo.com/core-java-tutorial/
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		String str="This ia a Sample String";
+		String words[]=str.split(" ");
+		List<String> chars=new ArrayList<>();
+		for(String word:words)
+		{
+			int len=word.length();
+			if(len>1)
+			{
+				chars.add(word.substring(len-2, len-1));
+			}
+		}
+		System.out.println("Input: "+str);
+		System.out.println("Output: "+String.join("",chars));
+	}
+}
