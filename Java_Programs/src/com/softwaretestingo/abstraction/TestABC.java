32c0c634612c9ddc7fb55a005771aa627ddeec89
SoftwareTestingO <softwaretestingblog.in@gmail.com>
2024-09-07 18:53:54 +0530

    Git Ignore File Updated

---
+++ Java_Programs/src/com/softwaretestingo/abstraction/TestABC.java
@@ -0,0 +1,33 @@
+package com.softwaretestingo.abstraction;
+
+public class TestABC 
+{
+	public static void main(String[] args) 
+	{
+		String input = "I Love Dogs";
+		generatePairs(input);
+	}
+
+	private static void generatePairs(String str) 
+	{
+		// Remove leading and trailing spaces and convert to uppercase
+		//str = str.trim().toUpperCase();
+		// Length of the string
+		int length = str.length();
+		// Loop through each character in the string
+		for (int i = 0; i < length; i++) 
+		{ 
+			// Print pairs for the current character at position i
+			for (int j = 0; j < length; j++) 
+			{
+				if (i != j) 
+				{
+					// Avoid pairing the character with itself
+					System.out.print(str.charAt(i) + "" + str.charAt(j) + ", ");
+				}
+			}
+			// Move to the next line after printing pairs for one character
+			System.out.println();
+		}
+	}
+}
